fa7ca33ea295385b22538a109ec44255796e78a7
sujji1729 <sujathanu96@gmail.com>
2024-08-05 23:04:02 +0530

    converting matter project create

---
+++ convert.js
@@ -0,0 +1,7 @@
+function convertMatter() {
+    document.getElementById("heading").textContent = "Dynamic Web Development";
+    document.getElementById("heading").style.color = "Blue";
+    document.getElementById("heading").style.fontFamily = "roboto";
+    document.getElementById("heading").style.textAlign = "center";
+
+}
